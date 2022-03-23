926cbb7ef4bb2be3dbcda5bc8de2b5012f0fcee9
Janani Denipitiya <it19556994@my.sliit.lk>
2022-03-23 22:39:41 +0530

    customer model

--- IT19556994/ElectroGrid/src/main/java/com/model/CustomerModel.java
+++ IT19556994/ElectroGrid/src/main/java/com/model/CustomerModel.java
@@ -22,5 +22,42 @@ public class CustomerModel {
 		return con;
 	}
 	
+	public String insertCustomer(String fname, String lname, String contact_number, String cus_email, int cus_status) {
+		String output = "";
+
+		try {
+			Connection con = connect();
+
+			if (con == null) {
+				return "Error while connecting to the database for inserting";
+
+			}
+
+			// create a prepared statement
+			String query = "insert into employees('fname','lname', 'contact_number', 'cus_email', 'cus_status') "
+					+ "values( ?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+
+			preparedStmt.setString(1, fname);
+			preparedStmt.setString(1, lname);
+			preparedStmt.setString(2, contact_number);
+			preparedStmt.setString(3, cus_email);
+			preparedStmt.setInt(4, 1);
+			
+
+			preparedStmt.execute();
+			con.close();
+
+			output = "Values Inserted Successfully";
+		} catch (Exception e) {
+			output = "Error while inserting the employee";
+			System.err.println(e.getMessage());
+		}
+		return output;
+
+	}
+	
 
 }
