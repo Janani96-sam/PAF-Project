b31615ffd120b02f30c6791bb328d7066ed5ea1b
NSansala <79787500+NSansala@users.noreply.github.com>
2022-04-22 18:37:01 +0530

    constant class implemented

---
+++ IT19962580/ConsumptionService/src/main/java/com/electrogrid/constant/Constant.java
@@ -0,0 +1,12 @@
+package com.electrogrid.constant;
+
+public class Constant {
+
+	public static int INDEX_ONE = 1;
+	public static int INDEX_TWO = 2;
+	public static int INDEX_THREE = 3;
+	public static int INDEX_FOUR = 4;
+	public static int INDEX_FIVE = 5;
+	public static int INDEX_SIX = 6;
+	
+}
