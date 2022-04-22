bc35076af2f1df0c3a761d828983d20f1b38ce90
NSansala <79787500+NSansala@users.noreply.github.com>
2022-04-22 18:39:31 +0530

    DB connection implemented

--- IT19962580/ConsumptionService/src/main/java/com/electrogrid/util/DatabaseConnection.java
+++ IT19962580/ConsumptionService/src/main/java/com/electrogrid/util/DatabaseConnection.java
@@ -1,5 +1,26 @@
 package com.electrogrid.util;
 
-public class DatabaseConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DatabaseConnection {
+	
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+    	Class.forName("com.mysql.cj.jdbc.Driver");
+    	//add local user name password to connect database
+    	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/consumptionservicedb","root","0919");
+    	 
+    	return con;
+    } 
+    
+    public static void main(String [] args) {
+    	try {
+			System.out.println(getConnection());
+		}
+    	catch(ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+    }
+    
 }
