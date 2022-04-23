0ddf243480a999fc94b1bb295db5d6007ef844d4
NSansala <79787500+NSansala@users.noreply.github.com>
2022-04-23 12:47:35 +0530

    get consumption by acc added

--- IT19962580/ConsumptionService/src/main/java/com/electrogrid/constant/ConsumptionConstant.java
+++ IT19962580/ConsumptionService/src/main/java/com/electrogrid/constant/ConsumptionConstant.java
@@ -2,10 +2,11 @@ package com.electrogrid.constant;
 
 public class ConsumptionConstant {
 	
-	public static String CREATECONSUMPTION = "INSERT INTO consumption (consumptionId,units,month,premisesId,readDate,accId) VALUES (?,?,?,?,?,?)";
+	public static String CREATECONSUMPTION = "INSERT INTO consumption (units,month,premisesId,readDate,accountId) VALUES (?,?,?,?,?)";
 	public static String GETALLCONSUMPTIONS = "SELECT * FROM consumption";
-	public static String GETCONSUMPTIONBYID = "SELECT * FROM consumption WHERE id = ?";
-	public static String UPDATECONSUMPTION = "UPDATE consumption SET units = ? , month = ? , premisesId = ? , readDate = ? , accId = ? WHERE consumptionId = ?";
+	public static String GETCONSUMPTIONBYID = "SELECT * FROM consumption WHERE consumptionId = ?";
+	public static String UPDATECONSUMPTION = "UPDATE consumption SET units = ? , month = ? , premisesId = ? , readDate = ? , accountId = ? WHERE consumptionId = ?";
 	public static String DELETECONSUMPTION = "DELETE FROM consumption WHERE consumptionId = ?";
+	public static String GETCONSUMPTIONBYACC = "SELECT * FROM consumption WHERE accountId = ?";
 
 }
