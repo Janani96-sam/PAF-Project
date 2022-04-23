42110b6f6c559e312824660d7c9342e06867ed31
Dimaji Himansala <dhimansala@gmail.com>
2022-04-23 16:40:08 +0530

    interruption constants implemented

--- IT19971872/InterruptionService/src/main/java/com/electrogrid/constant/InterruptionConstant.java
+++ IT19971872/InterruptionService/src/main/java/com/electrogrid/constant/InterruptionConstant.java
@@ -2,4 +2,10 @@ package com.electrogrid.constant;
 
 public class InterruptionConstant {
 
+	public static String CREATEINTERRUPTION = "INSERT INTO interruption (interruptionID,date,time,premisesId,area,reason,status) VALUES (?,?,?,?,?,?,?)";
+	public static String GETALLINTERRUPTIONS = "SELECT * FROM interruption";
+	public static String GETINTERRUPTIONBYID = "SELECT * FROM interruption WHERE interruptionID = ?";
+	public static String UPDATEINTERRUPTION = "UPDATE interruption SET date = ? , time = ? , premisesId = ? , area = ? , reason = ? , status = ?  WHERE interruptionID = ?";
+	public static String DELETEINTERRUPTION = "DELETE FROM interruption WHERE interruptionID = ?";
+	
 }
