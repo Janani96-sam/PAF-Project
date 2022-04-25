11b6dd75258a925f16afbd8f761c66960a1c2a81
ChamodiMandakini <78491451+ChamodiMandakini@users.noreply.github.com>
2022-04-25 21:43:08 +0530

    api new

--- IT19238340/ElectroGridComplain/build/classes/com/ComplainService.java
+++ IT19238340/ElectroGridComplain/build/classes/com/ComplainService.java
@@ -2,13 +2,13 @@ package com;
 
 import java.util.ArrayList;
 
-import javax.websocket.server.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -37,12 +37,13 @@ public class ComplainService {
 	}
 
 	@GET
-	@Path("/{name}/searchcomplains") // complain end point
+	@Path("/{search}/searchcomplains") // complain end point
 	@Produces(MediaType.APPLICATION_JSON)
-	public String searchComplains(@PathParam("name") String name) {
+	public String searchComplains(@PathParam("search") String search) {
 		ComplainModel em = new ComplainModel();
 		// String output = em.searchEmployees(name);
-		ArrayList<Complain> al = em.searchComplainsJson(name);
+		
+		ArrayList<Complain> al = em.searchComplainsJson(search);
 		Gson gson = new Gson();
 		String output = gson.toJson(al);
 		return output;
