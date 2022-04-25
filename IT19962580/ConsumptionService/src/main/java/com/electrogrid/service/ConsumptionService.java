2a56a739e4d7362bb18cedbf09ad04a899d1aef2
NSansala <79787500+NSansala@users.noreply.github.com>
2022-04-25 18:04:46 +0530

    updated

--- IT19962580/ConsumptionService/src/main/java/com/electrogrid/service/ConsumptionService.java
+++ IT19962580/ConsumptionService/src/main/java/com/electrogrid/service/ConsumptionService.java
@@ -21,11 +21,13 @@ public class ConsumptionService {
 		Connection con = DatabaseConnection.getConnection();
 		PreparedStatement preparedStatement = con.prepareStatement(create_query);
 			
-		preparedStatement.setInt(Constant.INDEX_ONE, consumption.getUnits());
-		preparedStatement.setString(Constant.INDEX_TWO, consumption.getMonth());
-		preparedStatement.setString(Constant.INDEX_THREE, consumption.getPremisesId());
-		preparedStatement.setString(Constant.INDEX_FOUR, consumption.getReadDate());
-		preparedStatement.setInt(Constant.INDEX_FIVE, consumption.getAccId());
+		preparedStatement.setInt(Constant.INDEX_ONE, consumption.getCurrentReading());
+		preparedStatement.setString(Constant.INDEX_TWO, consumption.getCurrentReadDate());
+		preparedStatement.setInt(Constant.INDEX_THREE, consumption.getPreviousReading());
+		preparedStatement.setString(Constant.INDEX_FOUR, consumption.getPreviousReadDate());
+		preparedStatement.setInt(Constant.INDEX_FIVE, consumption.getConsumedUnits());
+		preparedStatement.setString(Constant.INDEX_SIX, consumption.getMonth());
+		preparedStatement.setInt(Constant.INDEX_SEVEN, consumption.getAccountId());
 			  
 		boolean isCreated = preparedStatement.execute();
 		System.out.println(isCreated);
@@ -53,11 +55,13 @@ public class ConsumptionService {
 			Consumption consumption = new Consumption();
 				
 			consumption.setConsumptionId(rs.getInt(Constant.INDEX_ONE));
-			consumption.setUnits(rs.getInt(Constant.INDEX_TWO));
-			consumption.setMonth(rs.getString(Constant.INDEX_THREE));
-			consumption.setPremisesId(rs.getString(Constant.INDEX_FOUR));
-			consumption.setReadDate(rs.getString(Constant.INDEX_FIVE));
-			consumption.setAccId(rs.getInt(Constant.INDEX_SIX));
+			consumption.setCurrentReading(rs.getInt(Constant.INDEX_TWO));
+			consumption.setCurrentReadDate(rs.getString(Constant.INDEX_THREE));
+			consumption.setPreviousReading(rs.getInt(Constant.INDEX_FOUR));
+			consumption.setPreviousReadDate(rs.getString(Constant.INDEX_FIVE));
+			consumption.setConsumedUnits(rs.getInt(Constant.INDEX_SIX));
+			consumption.setMonth(rs.getString(Constant.INDEX_SEVEN));
+			consumption.setAccountId(rs.getInt(Constant.INDEX_EIGHT));
 				
 			clist.add(consumption);
 		}
@@ -80,13 +84,15 @@ public class ConsumptionService {
 				
 		while(rs.next()) {
 			int consumptionId = rs.getInt(Constant.INDEX_ONE);
-			int units = rs.getInt(Constant.INDEX_TWO);
-			String month = rs.getString(Constant.INDEX_THREE);
-			String premisesId = rs.getString(Constant.INDEX_FOUR);
-			String readDate = rs.getString(Constant.INDEX_FIVE);
-			int accId = rs.getInt(Constant.INDEX_SIX);
+			int currentReading = rs.getInt(Constant.INDEX_TWO);
+			String currentReadDate = rs.getString(Constant.INDEX_THREE);
+			int previousReading = rs.getInt(Constant.INDEX_FOUR);
+			String previousReadDate = rs.getString(Constant.INDEX_FIVE);
+			int consumedUnits = rs.getInt(Constant.INDEX_SIX);
+			String month = rs.getString(Constant.INDEX_SEVEN);
+			int accountId = rs.getInt(Constant.INDEX_EIGHT);
 			
-			consumption = new Consumption(consumptionId, units, month, premisesId, readDate, accId);
+			consumption = new Consumption(consumptionId, currentReading, currentReadDate, previousReading, previousReadDate, consumedUnits, month, accountId);
 		}
 		return consumption;
 				
@@ -99,13 +105,15 @@ public class ConsumptionService {
 		Connection con = DatabaseConnection.getConnection();
 		PreparedStatement preparedStatement = con.prepareStatement(update_query);
 				
-		preparedStatement.setInt(Constant.INDEX_ONE, consumption.getUnits());
-		preparedStatement.setString(Constant.INDEX_TWO, consumption.getMonth());
-		preparedStatement.setString(Constant.INDEX_THREE, consumption.getPremisesId());
-		preparedStatement.setString(Constant.INDEX_FOUR, consumption.getReadDate());
-		preparedStatement.setInt(Constant.INDEX_FIVE, consumption.getAccId());
+		preparedStatement.setInt(Constant.INDEX_ONE, consumption.getCurrentReading());
+		preparedStatement.setString(Constant.INDEX_TWO, consumption.getCurrentReadDate());
+		preparedStatement.setInt(Constant.INDEX_THREE, consumption.getPreviousReading());
+		preparedStatement.setString(Constant.INDEX_FOUR, consumption.getPreviousReadDate());
+		preparedStatement.setInt(Constant.INDEX_FIVE, consumption.getConsumedUnits());
+		preparedStatement.setString(Constant.INDEX_SIX, consumption.getMonth());
+		preparedStatement.setInt(Constant.INDEX_SEVEN, consumption.getAccountId());
 		
-		preparedStatement.setInt(Constant.INDEX_SIX, conId);
+		preparedStatement.setInt(Constant.INDEX_EIGHT, conId);
 				
 		if(preparedStatement.executeUpdate() > 0) {
 			return getConsumption(conId);
@@ -145,17 +153,19 @@ public class ConsumptionService {
 						
 		while(rs.next()) {
 			int consumptionId = rs.getInt(Constant.INDEX_ONE);
-			int units = rs.getInt(Constant.INDEX_TWO);
-			String month = rs.getString(Constant.INDEX_THREE);
-			String premisesId = rs.getString(Constant.INDEX_FOUR);
-			String readDate = rs.getString(Constant.INDEX_FIVE);
-			int accountId = rs.getInt(Constant.INDEX_SIX);
-					
-			consumption = new Consumption(consumptionId, units, month, premisesId, readDate, accountId);
+			int currentReading = rs.getInt(Constant.INDEX_TWO);
+			String currentReadDate = rs.getString(Constant.INDEX_THREE);
+			int previousReading = rs.getInt(Constant.INDEX_FOUR);
+			String previousReadDate = rs.getString(Constant.INDEX_FIVE);
+			int consumedUnits = rs.getInt(Constant.INDEX_SIX);
+			String month = rs.getString(Constant.INDEX_SEVEN);
+			int accountId = rs.getInt(Constant.INDEX_EIGHT);
+			
+			consumption = new Consumption(consumptionId, currentReading, currentReadDate, previousReading, previousReadDate, consumedUnits, month, accountId);
 		}
 		return consumption;
 						
-	}		
+	}	 	
 					
 		
 }
