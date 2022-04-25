04198d20780c2ee4eed12b5082349228b1a3e0d9
NSansala <79787500+NSansala@users.noreply.github.com>
2022-04-25 18:01:31 +0530

    updated

---
+++ IT19962580/ConsumptionService/src/main/java/com/electrogrid/service/ConsumptionInterService.java
@@ -0,0 +1,29 @@
+package com.electrogrid.service;
+
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+
+import com.electrogrid.model.Consumption;
+
+public class ConsumptionInterService {
+	
+	
+	public List<Consumption> retrieveAllAccounts() {
+		
+		Client client = ClientBuilder.newClient();
+	    
+		List<Consumption> consumptions =
+	            client.target("http://localhost:8080/customer/webapi/Customer")
+	            .path("all")
+	            .request(MediaType.APPLICATION_JSON)
+	            .get(new GenericType<List<Consumption>>() {
+	            });
+	    return consumptions;
+	    
+	}
+
+}
